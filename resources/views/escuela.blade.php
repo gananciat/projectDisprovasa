ae5fe5ff90141757f9c45382061a6231f317f1e2
Renato Ojeda <emisor.tarea@gmail.com>
2020-01-06 17:18:15 -0600

    Para halar cambios

--- resources/views/escuela.blade.php
+++ resources/views/escuela.blade.php
@@ -11,18 +11,10 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<<<<<<< HEAD
-  <link href="bundle2/css/app.027487afbc73c00dc9c4c438d0abb386.css" rel="stylesheet"></head>
-  <body class="sidebar-mini sidebar-collapse">
-    <div id="app"></div>
-    <!-- built files will be auto injected -->
-  <script type="text/javascript" src="bundle2/js/manifest.302b912dcb33a8c1fef2.js"></script><script type="text/javascript" src="bundle2/js/app.e1f5cbd936b0e4a08941.js"></script></body>
-=======
   <link href="bundle2/css/app.58646d90ee2693ba835f1b5793f68ce0.css" rel="stylesheet"></head>
   <body class="sidebar-mini sidebar-collapse">
     <div id="app"></div>
     <!-- built files will be auto injected -->
   <script type="text/javascript" src="bundle2/js/manifest.302b912dcb33a8c1fef2.js"></script><script type="text/javascript" src="bundle2/js/app.e86eb653a1f501c15c92.js"></script></body>
->>>>>>> 5bf7db29358c2612f54ddd47d69ea378f7d2592c
 </html>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,11 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-</head>
+<link href="bundle/css/app.0cd7054adc9d3e3def345a04f9928370.css" rel="stylesheet"></head>
+
 <body class="sidebar-mini layout-fixed text-sm">
     <div id="app"></div>
     <!-- built files will be auto injected -->
-</body>
+<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.8ee96ba1dbce7bd69842.js"></script></body>
+
 </html>
