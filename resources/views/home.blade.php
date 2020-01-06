ff62eb931a3b4a79b34698ee78f3a96d08c9bb15
Marlon <marlongf2195@gmail.com>
2020-01-05 21:37:22 -0600

    directory

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,11 +10,11 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<link href="bundle/css/app.6693b86416dfe32ae8250ec35577bf73.css" rel="stylesheet"></head>
+<link href="bundle/css/app.b80947b1b0e61573b75e0eda33fc7c8c.css" rel="stylesheet"></head>
 
-<body class="hold-transition sidebar-mini">
+<body class="sidebar-mini layout-fixed text-sm">
     <div id="app"></div>
     <!-- built files will be auto injected -->
-<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.e6df55b373386254f9ca.js"></script></body>
+<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.8db26ff96ac9900cfa51.js"></script></body>
 
 </html>
